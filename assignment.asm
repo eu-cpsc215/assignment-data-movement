7ec7e77dddfe9b0e2ea4bb386658fdc22c6b1de1
Victor Bushong <victorbush@gmail.com>
2023-11-03 21:26:12 -0500

    Update file names for consistency with other assignments.

---
+++ assignment.asm
@@ -0,0 +1,22 @@
+extrn ExitProcess : proc        ; Declare external function ExitProcess
+
+.DATA                           ; Directive; Enter .data section
+
+; Data here
+
+.CODE                           ; Directive: Enter .code section
+
+main PROC                       ; Directive: Begin function labeled `main`
+
+    sub rsp, 28h                ; Bump 8 bytes to ensure 16 byte alignment. Reserve 32 bytes shadow space.
+    ; -------------------- /\ PROLOGUE /\ --------------------
+
+    ; Code here
+
+    ; -------------------- \/ EPILOGUE \/ --------------------
+    xor rcx, rcx                ; Clear RCX
+    call ExitProcess            ; Use Windows API to exit the process
+
+main ENDP                       ; Directive: End function labeled `main`
+
+END
